f241f40314d8d467df6b0667a80e6938f3fb4cbd
AnasAitzouinet <anasaw236@gmail.com>
2022-10-10 17:41:12 +0100

    html_done

--- mainsite/urls.py
+++ mainsite/urls.py
@@ -2,5 +2,8 @@ from django.urls import path, re_path, include
 from . import views
 
 urlpatterns = [
-    path('',views.index, name="index")
+    path('',views.index, name="index"),
+    path('Home',views.home,name="Home"),
+    path('reserve',views.reserve,name="Reservations"),
+    path('Excursion',views.Excursion,name="Excursion")
 ]
--- mainsite/views.py
+++ mainsite/views.py
@@ -7,5 +7,14 @@ from django.template import RequestContext, loader
 
 # Create your views here.
 def index(request):
-    template = loader.get_template('base_Home.html')
+    template = loader.get_template('base_home.html')
     return HttpResponse(template.render({}, request))
+def home(request):
+    template = loader.get_template('Home/index.html')
+    return HttpResponse(template.render({},request))
+def reserve(request):
+    template = loader.get_template('reservations/reserver.html')
+    return HttpResponse(template.render({},request))
+def Excursion(request):
+    template = loader.get_template('Excursion/excursion.html')
+    return HttpResponse(template.render({},request))
